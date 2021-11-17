8b328c95045241bad385f102b7f9712bbeabca38
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-17 09:09:32 +0100

    Add references for Factory Method.

---
+++ factory_method/factory_method.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [refactoring_guru] Factory Method
+ *     https://refactoring.guru/design-patterns/factory-method/
+ *
+ *   [reddit20211117] Factory Method: Why create a separate class for Factory instead of just using the interface?
+ *     https://www.reddit.com/r/cpp/comments/qvsdu0/factory_method_why_create_a_separate_class_for/
+ */
