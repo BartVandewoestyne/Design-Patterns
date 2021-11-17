4fd402a8f48eeeb60dc0ef663d2d9f6636bc7b69
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-17 08:57:39 +0100

    Add references for Non Virtual Interface.

---
+++ non_virtual_interface/non_virtual_interface.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [morecppidioms] More C++ Idioms/Non-Virtual Interface
+ *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
+ *
+ *   [boccara20211117] Design Patterns VS Design Principles: Template Method
+ *     https://www.fluentcpp.com/2021/11/17/design-patterns-vs-design-principles-template-method/
+ */
