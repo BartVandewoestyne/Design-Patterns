b99616d4cbb9f31a7683b5b38303fc898cc9386e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-03-24 08:49:01 +0100

    Add reference for Visitor design pattern.

---
+++ visitor/visitor.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [boccara20220209] Design Patterns VS Design Principles: Visitor
+ *     https://www.fluentcpp.com/2022/02/09/design-patterns-vs-design-principles-visitor/
+ */
