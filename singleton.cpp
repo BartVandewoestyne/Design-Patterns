89f6c158f0f7299d5ec833df44e4b477cf86bbb3
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2025-01-21 14:23:24 +0100

    Add some reference for singleton.

--- singleton.cpp
+++ singleton.cpp
@@ -1,6 +1,15 @@
 /*
  * References:
  *
+ *   [cipriani20220704] Singleton: complete guide to understanding this controversy once and for all!
+ *     https://www.softplan.com.br/en/tech-writers/singleton-guia-completo-para-entender-essa-polemica-de-uma-vez-por-todas/
+ *
  *   [vorbrodt20220420] Singleton Pattern Explained (in C++)
  *     https://youtu.be/VTd7N2eRgNQ
+ *
+ *   [kofler20120108] Why Singletons Are Evil
+ *     https://blog.code-cop.org/2012/01/why-singletons-are-evil.html 
+ *
+ *   [hevery20080817] Singletons are Pathological Liars
+ *     https://testing.googleblog.com/2008/08/by-miko-hevery-so-you-join-new-project.html
  */
